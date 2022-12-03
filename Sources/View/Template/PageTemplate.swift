db16a0dda3437d51edd6dedd89eb4a18128d8b36
Alex Skorulis <skorulis@gmail.com>
2022-12-03 16:21:53 +1100

    Refactor

---
+++ Sources/View/Template/PageTemplate.swift
@@ -0,0 +1,54 @@
+//Created by Alexander Skorulis on 3/12/2022.
+
+import Foundation
+import SwiftUI
+
+// MARK: - Memory footprint
+
+public struct PageTemplate<Nav: View, Content: View> {
+    
+    private let nav: () -> Nav
+    private let content: () -> Content
+    
+    public init(nav: @escaping () -> Nav, content: @escaping () -> Content) {
+        self.nav = nav
+        self.content = content
+    }
+    
+}
+
+// MARK: - Rendering
+
+extension PageTemplate: View {
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            nav()
+            ScrollView {
+                Spacer()
+                    .frame(height: 12)
+                content()
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+// MARK: - Previews
+
+struct PageTemplate_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        PageTemplate {
+            NavBar(left: .back({}), mid: .title("Title"))
+        } content: {
+            Text("TEst")
+            
+        }
+
+    }
+}
+
+
+
